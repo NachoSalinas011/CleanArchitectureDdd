5f184f3fd972fb9c7248bb615aa0783ca441b03b
Nicolas Ignacio Salinas <nicolasignacio.salinas@externos-cl.cencosud.com>
2023-03-06 09:30:14 -0300

    Agregar archivos de proyecto.

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data source=localhost\SQLEXPRESS; Initial Catalog=Streamer; Integrated Security=True; TrustServerCertificate=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        //Esto se utiliza en casos por ej: Foreign key no se llama cómo la tabla referenciada, etc
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Video>()
+                .HasMany(x => x.Actores)
+                .WithMany(x => x.Videos)
+                .UsingEntity<VideoActor>(
+                    a => a.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+
+        }
+
+        public DbSet<Streamer> Streamers { get; set; }
+        public DbSet<Video> Videos { get; set; }
+    }
+}
---
+++ CleanArchitecture.Domain/Actor.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain
+{
+    public class Actor : BaseDomainModel
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public virtual ICollection<Video> Videos { get; set; }
+
+        public Actor()
+        {
+            Videos = new HashSet<Video>();
+        }
+    }
+}
